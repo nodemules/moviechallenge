647d42018b294a6627788541789a8b744aa70058
drscogin <drscogin@gmail.com>
2015-07-26 21:42:59 -0400

    Reworked search functionality through Angular

---
+++ public/javascripts/search.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('myApp', [])
+  .controller('MovieController', function($scope, $http) {
+    var pendingTask;
+/*
+    $scope.search1 = "Alien";
+    $scope.search2 = "Jaws";*/
+
+  if ($scope.search1 === undefined) {
+      $scope.search1 = "Alien";
+    }
+    if ($scope.search2 === undefined) {
+      $scope.search2 = "Jaws";
+    }
+      fetch();
+    $scope.change = function() {
+      if (pendingTask) {
+        clearTimeout(pendingTask);
+      }
+      pendingTask = setTimeout(fetch, 800);
+    };
+
+    function fetch() {
+      $http.get("http://www.omdbapi.com/?t=" + $scope.search1 + "&tomatoes=true&plot=full")
+        .success(function(response) {
+          $scope.details1 = response;
+        });
+
+      $http.get("http://www.omdbapi.com/?t=" + $scope.search2 + "&tomatoes=true&plot=full")
+        .success(function(response) {
+          $scope.details2 = response;
+        });
+    }
+
+/*    $scope.update = function(movie) {
+      $scope.search = movie.Title;
+      $scope.change();
+    };*/
+
+/*    $scope.select = function() {
+      this.setSelectionRange(0, this.value.length);
+    }*/
+  });
