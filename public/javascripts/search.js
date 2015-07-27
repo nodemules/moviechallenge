75c84e6b8e3b968704979b0fe0fbb39efcefe84a
drscogin <drscogin@gmail.com>
2015-07-26 23:31:21 -0400

    moved pseudocode out of MovieController app

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -13,37 +13,7 @@ angular.module('myApp', [])
     if ($scope.search2 === undefined) {
       $scope.search2 = "Jaws";
     }
-      fetch();
-
-    //TODO: Get current locked in movie for Jay
-    // Get current locked in movie for Brent
-    //If no current movie display nothing
-    //How to get current locked in movie
-    //fetch
-    //db.moviearchive.find(most recent id with locked flag = 1){ get title
-    //  scope.search1 = title
-
-/*router.get('/getlastid', function(req, res) {
-    var db = req.db;
-   
-    var collection = db.get('postings');
-    console.log("getlastid");
-    collection.find({},{limit: 1, sort: {_id: -1}},function(e,docs){
-        res.json(docs);
-    });
-});*/
-
-
-      //}
-      //fetch();
-      var collection = db.get('moviearchive');
-      collection.find({ movieid: 1 } , function(e,docs){
-        res.json(docs);
-           alert(res.jason(docs));
-    });
-
-
-    
+      fetch();    
 
     $scope.change = function() {
       if (pendingTask) {
@@ -63,11 +33,6 @@ angular.module('myApp', [])
           $scope.details2 = response;
         });
     }
-
-     //Lock in button:
-     //Store movie selection in mongo
-     //db.moviearchive.insert(next available id, $scope.search1, user)
-
 
 /*    $scope.update = function(movie) {
       $scope.search = movie.Title;
