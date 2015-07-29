3ea81398db33bd38995caa9f5627ec956180605c
drscogin <drscogin@gmail.com>
2015-07-29 08:16:25 -0400

    Added angular functionality to post search to db

--- public/javascripts/controllers/search.js
+++ public/javascripts/controllers/search.js
@@ -5,6 +5,8 @@ angular.module('SearchApp', [])
       var pendingTask;
       fetch();   
 
+
+
     $scope.change = function() {
       if (pendingTask) {
         clearTimeout(pendingTask);
@@ -14,17 +16,32 @@ angular.module('SearchApp', [])
 
     function fetch() {
       $http.get("http://www.omdbapi.com/?t=" + $scope.search1 + "&tomatoes=true&plot=full")
-        .success(function(response) {
-          
+        .success(function(response) {          
           $scope.details1 = response;
-
         });
 
       $http.get("http://www.omdbapi.com/?t=" + $scope.search2 + "&tomatoes=true&plot=full")
         .success(function(response) {
           $scope.details2 = response;
         });
-    }
-  
+    };
+      
+      //var $scope.details1;
+
+    $scope.saveMovie1 = function() {
+      var movietitle = {
+              name : $scope.search1
+          }
+
+
+      $http.post("/api/movies", movietitle)
+        .success(function(response) {
+          return response;
+/*          if (err)
+            res.send(err);
+
+          res.json({ message: 'Movie saved!' });*/
+        });
+    };      
 
   });
