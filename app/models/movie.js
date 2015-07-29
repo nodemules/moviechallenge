a5bcf5d201b7e9b102828b15376edd84a0886159
staticsound <johngreedjr@gmail.com>
2015-07-29 00:37:29 -0400

    fixing commit merge disaster

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // app/models/movie.js
 
 var mongoose			= require('mongoose');
@@ -7,17 +6,5 @@ var	Schema 				= mongoose.Schema;
 var	MovieSchema			= new Schema({
 		name: String	
 	});
-=======
-// app/models/movies.js
-
-var mongoose 	= require('mongoose');
-var Schema		= mongoose.Schema;
-
-var MovieSchema	= new Schema(
-	{
-    title : {type: String, default: ' '},
-    owner : {type: String, default: ' '}
-});
->>>>>>> ecabc223286e9a421559c9c0de610d9663fe1a91
 
 module.exports = mongoose.model('Movie', MovieSchema);
