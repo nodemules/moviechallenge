0b3e19bc52deee6d24f7498b77a33d931d361a17
staticsound <johngreedjr@gmail.com>
2015-08-28 15:04:17 -0400

    new mongolabs db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
  module.exports = {
 
+ 	//Development
+ 	url : 'mongodb://dbuser:dbpass@ds035693.mongolab.com:35693/moviechallenges_dev'
+
 	// the database url to connect
-	url : 'mongodb://dbuser:dbpass@ds033153.mongolab.com:33153/movies_test_1-1-1'
+	//  url : 'mongodb://dbuser:dbpass@ds033153.mongolab.com:33153/movies_test_1-1-1'
 
 	// 1.0 development url
 	// url : 'mongodb://moviechallenger:password@ds061611.mongolab.com:61611/moviechallenges'
